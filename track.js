eb8d1de9751e853854b90b4d36b00e3aa1d8cfb9
Bai-ee <bryanballi@gmail.com>
2022-07-27 10:54:45 -0500

    WIP

---
+++ track.js
@@ -0,0 +1,30 @@
+
+
+const trackDir = "";
+const bpm = 120;
+
+const parts = [
+
+    { 
+        //update "file" name to match the name of your file
+        file: "Bai-ee_Acid_Beach_(RADIO_EDIT).wav", 
+        length: 200, 
+        // loop: 1
+
+    },
+];
+
+const presets = [];
+presets.push([1,]);
+const boxHeight = 50;
+const reverseScrolling = false;
+
+const downloadName = "Bai-ee_(Loop_1)_Genesis_Collection.wav"
+console.log("ABOUT: Open Source Interactive Music NFT with integrated token gate and direct download mechanism.")
+console.log("This players code and assets can be downloaded and customized at edittrax.nft");
+console.log("DOWNLOAD LIMITATIONS:")
+console.log("• Original artist retains all creative right")
+console.log("• You are fully encouraged to use in your mix tapes, social content creation and public performances")
+console.log("• You are not allowed to distribute downloaded material for direct sale or distribution in any way")
+console.log("CONTACT: edittrax@protonmail.com")
+console.log("DEVELOPER: Bai-ee")
